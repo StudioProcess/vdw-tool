8a84c72b3c8b69425b28887ebd4466af93029c96
Stefan Kernjak <stefan@thisotherthing.co>
2018-09-11 22:06:00 +0200

    got text to svg into physics and update svgs mostly working

---
+++ components/textViewer/textPhysics.ts
@@ -0,0 +1,309 @@
+import decomp from "poly-decomp";
+import {
+  Engine,
+  Render,
+  World,
+  Bodies,
+  Common,
+  Body,
+  Svg,
+  Vertices,
+} from "matter-js";
+
+let pathseg = null;
+
+const borderWidth = 100;
+const heightBorderDistance = 1000 + borderWidth * 0.5;
+
+const worldBounds = {
+  width: 100,
+  height: heightBorderDistance * 2 - borderWidth,
+};
+
+export default class CirclePhysics {
+
+  private engine: Engine;
+  private world: World;
+  private render: Render;
+
+  private topBorder: Body;
+  private leftBorder: Body;
+  private rightBorder: Body;
+  private bottomBorder: Body;
+
+  private bodies: Body[];
+  private xOffsets: number[];
+  private yOffsets: number[];
+
+  public setup(
+    aspectRatio: number,
+  ) {
+    if (pathseg === null) {
+      pathseg = require("pathseg");
+      window.decomp = decomp;
+    }
+
+    this.engine = Engine.create();
+    this.world = this.engine.world;
+
+    this.topBorder = Bodies.rectangle(
+      0,
+      -heightBorderDistance,
+      10000,
+      borderWidth,
+      {
+        isStatic: true,
+      },
+    );
+    this.leftBorder = Bodies.rectangle(
+      -heightBorderDistance,
+      0,
+      borderWidth,
+      10000,
+      {
+        isStatic: true,
+      },
+    );
+    this.rightBorder = Bodies.rectangle(
+      heightBorderDistance,
+      0,
+      borderWidth,
+      10000,
+      {
+        isStatic: true,
+      },
+    );
+    this.bottomBorder = Bodies.rectangle(
+      0,
+      heightBorderDistance,
+      10000,
+      borderWidth,
+      {
+        isStatic: true,
+      },
+    );
+
+    this.onResize(aspectRatio);
+
+    World.add(
+      this.world,
+      [
+        this.topBorder,
+        this.leftBorder,
+        this.rightBorder,
+        this.bottomBorder,
+      ],
+    );
+
+    this.bodies = [];
+    this.bodies.push(
+      Bodies.circle(0, 0, 500, { restitution: 0.6, friction: 0.1 }),
+    );
+
+    this.xOffsets = [];
+    this.yOffsets = [];
+
+    World.add(
+      this.world,
+      this.bodies,
+    );
+
+    if (true) {
+      this.render = Render.create({
+        element: document.body,
+        engine: this.engine,
+        options: {
+          width: 2000,
+          height: 1200,
+          // showAngleIndicator: true,
+        },
+      });
+
+      Render.run(this.render);
+
+      Render.lookAt(
+        this.render,
+        {
+          min: { x: -1500, y: -1300 },
+          max: { x: 1500, y: 1300 },
+        },
+      );
+
+      this.render.canvas.style.position = "absolute";
+      this.render.canvas.style.top = "10px";
+      this.render.canvas.style.left = "10px";
+      this.render.canvas.style.opacity = "0.8";
+      this.render.canvas.style.transformOrigin = "left top";
+      this.render.canvas.style.transform = "scale(0.5)";
+      document.body.appendChild(this.render.canvas);
+    }
+  }
+
+  public getWorldBounds = () => {
+    return worldBounds;
+  }
+
+  public onResize(aspectRatio) {
+    worldBounds.width = Math.round(heightBorderDistance * aspectRatio * 2 - borderWidth);
+
+    Body.setPosition(
+      this.leftBorder,
+      {
+        x: heightBorderDistance * -aspectRatio,
+        y: 0,
+      },
+    );
+
+    Body.setPosition(
+      this.rightBorder,
+      {
+        x: heightBorderDistance * aspectRatio,
+        y: 0,
+      },
+    );
+  }
+
+  public clearBodies = () => {
+    for (let i = 0, l = this.bodies.length; i < l; i++) {
+      World.remove(
+        this.world,
+        this.bodies[i],
+      );
+    }
+
+    this.bodies.length = 0;
+
+    this.xOffsets.length = 0;
+    this.yOffsets.length = 0;
+  }
+
+  public setFromSVGs(svgs: SVGElement[]) {
+    const tmpSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg",
+    );
+
+    for (let i = 0, l = svgs.length; i < l; i++) {
+      const path = (svgs[i].childNodes[0] as SVGGElement).cloneNode(true).childNodes[0] as SVGPathElement;
+
+      const leftOffset = parseFloat(path.getAttribute("leftoffset"));
+
+      let vertices = Svg.pathToVertices(path, 5);
+
+      let minX = Number.POSITIVE_INFINITY;
+      let minY = Number.POSITIVE_INFINITY;
+
+      for (let j = 0, l2 = vertices.length; j < l2; j++) {
+          if (vertices[j].x < minX) {
+            minX = vertices[j].x;
+          }
+          if (vertices[j].y < minY) {
+            minY = vertices[j].y;
+          }
+      }
+      minX += leftOffset;
+
+      let body = Bodies.fromVertices(
+        0,
+        0,
+        vertices,
+        {
+          // isStatic: true,
+        },
+        true,
+      );
+
+      if (body === undefined) {
+        // remove all but first vertex line, so there aren't holes
+        const matches = path.getAttribute("d").match(/M[0-9A-Z.\s]*?[ZM]/);
+        matches.sort((a, b) => {
+          return b.length - a.length;
+        });
+        const pathD = matches[0];
+        path.setAttribute("d", pathD);
+        vertices = Svg.pathToVertices(path, 5);
+
+        minX = Number.POSITIVE_INFINITY;
+        minY = Number.POSITIVE_INFINITY;
+
+        for (let j = 0, l2 = vertices.length; j < l2; j++) {
+            if (vertices[j].x < minX) {
+              minX = vertices[j].x;
+            }
+            if (vertices[j].y < minY) {
+              minY = vertices[j].y;
+            }
+        }
+        minX += leftOffset;
+
+        body = Bodies.fromVertices(
+          0,
+          0,
+          vertices,
+          {
+            // isStatic: true,
+          },
+          true,
+        );
+      }
+      this.bodies.push(body);
+
+      const xOffset = this.bodies[this.bodies.length - 1].bounds.min.x - minX;
+      const yOffset = this.bodies[this.bodies.length - 1].bounds.min.y - minY;
+      Body.setPosition(this.bodies[this.bodies.length - 1], {x: -xOffset, y: -yOffset});
+
+      svgs[i].childNodes[0].childNodes[0].setAttribute(
+        "transform",
+        `translate(${leftOffset + xOffset} ${yOffset})`,
+      );
+    }
+
+    World.add(
+      this.world,
+      this.bodies,
+    );
+  }
+
+  public makeNonStatic() {
+    for (let i = 0, l = this.bodies.length; i < l; i++) {
+      Body.setStatic(
+        this.bodies[i],
+        false,
+      );
+    }
+  }
+
+  public update(svgs: SVGElement[]) {
+    Engine.update(this.engine);
+
+    // let baseIndex = 0;
+    // for (let i = 0, l = this.bodies.length; i < l; i++) {
+    //   baseIndex = i * 3;
+    //   circlesDataBuffer.array[baseIndex] = this.bodies[i].position.x * 0.001;
+    //   circlesDataBuffer.array[baseIndex + 1] = this.bodies[i].position.y * -0.001;
+    //   circlesDataBuffer.array[baseIndex + 2] = this.bodies[i].circleRadius * 0.001;
+    // }
+
+    for (let i = 0, l = this.bodies.length; i < l; i++) {
+      // (svgs[i].childNodes[0] as SVGPathElement).style.transform = `translateX(${
+      //   ((this.bodies[i].position.x + this.xOffsets[i]) / worldBounds.width + 0.5) * 100
+      // }vw) translateY(${
+      //   ((this.bodies[i].position.y + this.yOffsets[i]) / worldBounds.height + 0.5) * 100
+      // }vh) rotate(${
+      //   this.bodies[i].angle * 57.2958
+      // }deg)`;
+      (svgs[i].childNodes[0] as SVGPathElement).setAttribute(
+        "transform",
+        `translate(${
+          this.bodies[i].position.x
+        } ${
+          this.bodies[i].position.y
+        }) rotate(${
+          this.bodies[i].angle * 57.2958
+        })`,
+      );
+    }
+
+    // circlesDataBuffer.needsUpdate = true;
+  }
+}
--- components/textViewer/textViewer.tsx
+++ components/textViewer/textViewer.tsx
@@ -7,14 +7,19 @@ import {
 
 import * as OpenType from "opentype.js";
 
+import TextPhysics from "./textPhysics";
+
 export default class StatueViewer extends Component<any, any> {
     private clock: Clock;
+    private textPhysics: TextPhysics;
 
     private frameId: number;
 
     private font: any;
 
     private containerRef: HTMLDivElement;
+
+    private svgs: SVGElement[][];
 
   constructor(props: any) {
     super(props);
@@ -29,6 +34,8 @@ export default class StatueViewer extends Component<any, any> {
     this.clock = new Clock();
     this.clock.start();
 
+    this.svgs = [];
+
     OpenType.load(
       "/static/fonts/Linotype - NHaasGroteskTXPro-55Rg.otf",
       (err, font) => {
@@ -37,14 +44,27 @@ export default class StatueViewer extends Component<any, any> {
         } else {
           this.font = font;
 
+          this.svgs.push([]);
+
           this.generateSVGs(
-            "TestghHx",
+            "TestXHg890ijxe",
             this.font,
             this.containerRef,
+            this.svgs[0],
+          );
+
+          this.textPhysics.setFromSVGs(this.svgs[0]);
+
+          this.textPhysics.update(
+            this.svgs[0],
           );
         }
       },
     );
+
+    this.textPhysics = new TextPhysics();
+    this.textPhysics.setup(window.innerWidth / window.innerHeight);
+    this.textPhysics.clearBodies();
 
     window.addEventListener("resize", this.onResize);
     this.onResize();
@@ -62,12 +82,15 @@ export default class StatueViewer extends Component<any, any> {
     text: string,
     font: any,
     container: HTMLDivElement,
+    svgs: SVGElement[],
   ) {
     const fontPath = this.font.getPath(text);
     const pathBounds = fontPath.getBoundingBox();
-    const viewBox = `${0} ${0} ${pathBounds.x2 - pathBounds.x1 + 6} ${pathBounds.y2 - pathBounds.y1 + 12}`;
+    // const viewBox = `${0} ${0} ${pathBounds.x2 - pathBounds.x1 + 6} ${pathBounds.y2 - pathBounds.y1 + 12}`;
 
     let leftDistance = 0.0;
+
+    const physicsBounds = this.textPhysics.getWorldBounds();
 
     for (let i = 0, l = text.length; i < l; i++) {
       const svg = document.createElementNS(
@@ -77,38 +100,54 @@ export default class StatueViewer extends Component<any, any> {
       svg.style.position = "absolute";
       svg.style.top = "0";
       svg.style.left = "0";
+      svg.style.width = "100vw";
+      svg.style.height = "100vh";
 
       if (i > 0) {
         leftDistance = font.getAdvanceWidth(text.substring(0, i + 1));
         leftDistance -= font.getAdvanceWidth(text[i]);
       }
 
-      const charPath = this.font.getPath(text[i], leftDistance, 60);
+      const charPath = this.font.getPath(text[i], 0, 60);
 
-      svg.setAttribute("viewBox", viewBox);
-      svg.innerHTML = `<path
-        fill="none"
-        stroke-width="1"
-        stroke="black"
-        d="${charPath.toPathData(2)}"
-      />`;
-      this.containerRef.appendChild(svg);
+      // svg.setAttribute("viewBox", viewBox);
+      svg.setAttribute("viewBox", `${
+        physicsBounds.width * -0.5
+      } ${
+        physicsBounds.height * -0.5
+      } ${
+        physicsBounds.width
+      } ${
+        physicsBounds.height
+      }`);
+
+      svg.innerHTML = `<g><path
+          fill="none"
+          stroke-width="1"
+          stroke="black"
+          leftoffset="${leftDistance}"
+          d="${charPath.toPathData(2)}"
+        /></g>`;
+      svgs.push(svg);
+      container.appendChild(svg);
     }
   }
 
   private onResize = () => {
+    this.textPhysics.onResize(
+      window.innerWidth / window.innerHeight,
+    );
     // this.circlePhysics.onResize(
     //   window.innerWidth / window.innerHeight,
     // );
   }
 
-
   private animate = () => {
     const delta = Math.min(1.0 / 20.0, this.clock.getDelta());
 
-    // this.circlePhysics.update(
-    //   this.circlesDataBuffer,
-    // );
+    this.textPhysics.update(
+      this.svgs[0],
+    );
 
     this.draw();
 
