c7ddfcd8d408d62dc70eec467055801dc83ccb02
RESL <moritz@resl.xyz>
2018-09-16 20:05:40 +0200

    Debug physics window hidden, Default Text changed

--- components/textViewer/textViewer.tsx
+++ components/textViewer/textViewer.tsx
@@ -48,7 +48,7 @@ export default class StatueViewer extends Component<any, any> {
 
           this.svgs = [];
 
-          this.newText("TestxHije8\ntest");
+          this.newText("A City\nFull Of\nDesign");
         }
       },
     );
